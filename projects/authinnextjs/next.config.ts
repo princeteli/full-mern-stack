3674485fa1392fd49915933855ecda502546173a
princeteli <pt0000100@gmail.com>
2026-02-07 12:27:21 +0530

    Add files via upload

---
+++ projects/authinnextjs/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+import { hostname } from "os";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images:{
+    remotePatterns:[
+     { hostname:"lh3.googleusercontent.com"}
+    ]
+  }
+};
+
+export default nextConfig;
